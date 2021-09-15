a0d2d6c875214290324847539cfaeb4045efd5f2
Analis12 <anitalis12@gmail.com>
2021-09-14 20:14:31 -0500

    componentes angular

--- Frontend/gestion-veterinaria/src/app/app-routing.module.ts
+++ Frontend/gestion-veterinaria/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'inicio', component: HomeComponent},
+  {path: 'login', component: LoginComponent},
+  { path: '', redirectTo: 'inicio', pathMatch: 'full'},
+  { path: '**', redirectTo: 'inicio'}
+  //{ path: 'dashboard', loadChildren: () => import('./components/dashboard/dashboard.module').then(m => m.DashboardModule),canActivate: [ AuthGuard ], data: {permittedRoles: ['A','T']}  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
